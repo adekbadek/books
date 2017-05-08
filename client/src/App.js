219dd5f700bd1dd3f9774b5d593c4ff2b8f2b194
Adam Borowski <adam@adamboro.com>
2017-05-08 17:04:17 -0400

    fetch and display books from rails API

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,29 @@
 import React, { Component } from 'react'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      books: [],
+    }
+  }
+  componentDidMount () {
+    // TODO: fetch polyfill
+    fetch('/api/books')
+      .then(res => res.json())
+      .then(books => {
+        this.setState({books})
+      })
+  }
   render () {
     return (
       <div>
-        <h1>hello</h1>
+        <h1>books</h1>
+        {this.state.books.map(book => {
+          return (
+            <p key={book.id}>{book.title}</p>
+          )
+        })}
       </div>
     )
   }
