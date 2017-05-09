dc0587dc75f9876415321d4eb76e9c754b24436e
Adam Borowski <adam@adamboro.com>
2017-05-08 20:18:31 -0400

    react - create book form

--- client/src/App.js
+++ client/src/App.js
@@ -3,17 +3,41 @@ import React, { Component } from 'react'
 class App extends Component {
   constructor () {
     super()
+    this.setInputState = this.setInputState.bind(this)
+    this.submit = this.submit.bind(this)
+    this.refreshBooks = this.refreshBooks.bind(this)
     this.state = {
       books: [],
+      inputVal: '',
     }
   }
-  componentDidMount () {
-    // TODO: fetch polyfill
+  setInputState (e) {
+    this.setState({inputVal: e.target.value})
+  }
+  submit (e) {
+    e.preventDefault()
+
+    fetch('api/create', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: this.state.inputVal,
+      })
+    })
+      .then(res => res.json())
+      .then(() => {
+        this.refreshBooks()
+        this.setState({inputVal: ''})
+      })
+  }
+  refreshBooks () {
     fetch('/api/books')
       .then(res => res.json())
       .then(books => {
         this.setState({books})
       })
+  }
+  componentDidMount () {
+    this.refreshBooks()
   }
   render () {
     return (
@@ -24,6 +48,10 @@ class App extends Component {
             <p key={book.id}>{book.title}</p>
           )
         })}
+        <form onSubmit={this.submit}>
+          <input type='text' value={this.state.inputVal} onChange={this.setInputState} />
+          <input type='submit' value='add' />
+        </form>
       </div>
     )
   }
