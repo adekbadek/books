2024bed00031a8082eec3e58f92900d4826e0ce6
Adam Borowski <adam@adamboro.com>
2017-05-09 21:18:16 -0400

    better seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,17 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-books = ['The Redux Book', 'SlaughterHouse Five', 'Sapiens: A Brief History of Humankind']
+books = ['The Redux Book', 'Slaughterhouse Five', 'Sapiens: A Brief History of Humankind', 'Widnokrąg', 'Ciemno, prawie noc']
 
 books.each do |book|
-  Book.create(title: book)
+  end_date = Time.now - (Random.new.rand * 200).days
+
+  Book.create(
+    title: book,
+    start_date: end_date - (Random.new.rand * 20 + 5).days,
+    end_date: end_date,
+    rep_1: end_date + 10.days,
+    rep_2: end_date + 30.days,
+    rep_3: end_date + 60.days
+  )
 end
