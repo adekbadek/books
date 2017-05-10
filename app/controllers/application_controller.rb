12e2383bf62a153927bffac8f07d7d34c84a071c
Adam Borowski <adam@adamboro.com>
2017-05-09 22:01:10 -0400

    rails - add_reps helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+  def self.add_reps(book)
+    book.rep_1 = book.end_date + 10.days
+    book.rep_2 = book.end_date + 30.days
+    book.rep_3 = book.end_date + 60.days
+
+    book.save
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,12 +11,11 @@ books = ['The Redux Book', 'Slaughterhouse Five', 'Sapiens: A Brief History of H
 books.each do |book|
   end_date = Time.now - (Random.new.rand * 200).days
 
-  Book.create(
+  new_book = Book.create(
     title: book,
     start_date: end_date - (Random.new.rand * 20 + 5).days,
-    end_date: end_date,
-    rep_1: end_date + 10.days,
-    rep_2: end_date + 30.days,
-    rep_3: end_date + 60.days
+    end_date: end_date
   )
+
+  ApplicationController.add_reps(new_book)
 end
