e045096666ebc5652b6d1afbee1bd2f5ac747f04
Adam Borowski <adam@adamboro.com>
2017-05-10 15:53:19 -0400

    Table - className prop; tables styles

--- client/src/Table.jsx
+++ client/src/Table.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props =>
   <div className='pv4'>
     <div className='overflow-auto'>
-      <table className='f6 w-100 mw8 center' cellSpacing='0'>
+      <table className={`f6 w-100 mw8 center ${props.className}`} cellSpacing='0'>
         <thead>
           <tr>
             {props.headers.map((t, i) => <th key={i} className='bb b--black-20 tl pb2 pr3'>{t}</th>)}
