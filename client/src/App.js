54d846eede097068865cf3b7617628afc5a707a0
Adam Borowski <adam@adamboro.com>
2017-05-10 21:15:14 -0400

    add react-dom-router and set up index route with Main view

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 import Main from 'views/Main'
 
 class App extends React.Component {
   render () {
     return (
-      <div>
-        <Main />
-      </div>
+      <Router>
+        <div>
+          <Route exact path='/' component={Main} />
+        </div>
+      </Router>
     )
   }
 }
