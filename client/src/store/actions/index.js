9307a60f48875fa915816fd9f5e03b3c263b3669
Adam Borowski <adam@adamboro.com>
2017-05-11 19:08:51 -0400

    state - displayLoader

--- client/src/store/actions/index.js
+++ client/src/store/actions/index.js
@@ -2,3 +2,8 @@ export const setFlashMessage = message => ({
   type: 'SET_FLASH_MESSAGE',
   message,
 })
+
+export const setLoaderState = displayLoader => ({
+  type: 'SET_LOADER_STATE',
+  displayLoader,
+})
--- client/src/store/reducers/uiReducer.js
+++ client/src/store/reducers/uiReducer.js
@@ -1,11 +1,14 @@
 const initialState = {
   message: null,
+  displayLoader: null,
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case 'SET_FLASH_MESSAGE':
       return Object.assign({}, state, {message: action.message})
+    case 'SET_LOADER_STATE':
+      return Object.assign({}, state, {displayLoader: action.displayLoader})
     default:
       return state
   }
