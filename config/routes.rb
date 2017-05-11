5bee60706c171a1c8f6fafef870a9b13bc79ecd9
Adam Borowski <adam@adamboro.com>
2017-05-10 20:42:38 -0400

    user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  post 'authenticate', to: 'authentication#authenticate'
+
   scope '/api' do
     get :books, to: 'books#index'
     post :books, to: 'books#create'
