89b9a8b01b572bd2cabb00b4b2ce6412bb4bc102
Adam Borowski <adam@adamboro.com>
2017-05-10 20:52:04 -0400

    seeds tweak

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,14 +8,17 @@
 
 books = ['The Redux Book', 'Slaughterhouse Five', 'Sapiens: A Brief History of Humankind', 'Widnokrąg', 'Ciemno, prawie noc']
 
-books.each do |book|
+books.each_with_index do |book, i|
   end_date = Time.now - (Random.new.rand * 200).days
 
   new_book = Book.create(
     title: book,
-    start_date: end_date - (Random.new.rand * 20 + 5).days,
-    end_date: end_date
+    start_date: end_date - (Random.new.rand * 20 + 5).days
   )
+
+  if i != 0
+    new_book.end_date = end_date    
+  end
 
   ApplicationController.change_reps(new_book)
 end
