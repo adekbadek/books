545d50c4b88191c6778dae49aaa00e94ec824212
Adam Borowski <adam@adamboro.com>
2017-05-10 20:33:44 -0400

    JsonWebToken class - encode & decode JWT

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module ReadRails
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # load lib dir
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
