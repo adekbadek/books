9e46b73cf2bf12d7bf6ffc5f0a6d97f9336ca76b
Adam Borowski <adam@adamboro.com>
2017-05-11 19:10:00 -0400

    API request helper - set displayLoader state on fetching

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,3 +1,6 @@
+import store from 'store'
+import { setLoaderState } from 'store/actions'
+
 const LOCAL_STOREGE_ITEM = 'JWT'
 
 export const saveCredentials = token => localStorage.setItem(LOCAL_STOREGE_ITEM, token)
@@ -7,6 +10,7 @@ export const readCredentials = () => localStorage.getItem(LOCAL_STOREGE_ITEM)
 export const revokeCredentials = () => localStorage.removeItem(LOCAL_STOREGE_ITEM)
 
 export const request = ({url, method, data}) => {
+  store.dispatch(setLoaderState(true))
   const conf = {
     method: method || 'GET',
     headers: {
@@ -16,5 +20,8 @@ export const request = ({url, method, data}) => {
   if (method !== 'GET' && data) {
     conf.body = JSON.stringify(data)
   }
-  return fetch(url, conf)
+  return fetch(url, conf).then(v => {
+    store.dispatch(setLoaderState(null))
+    return v
+  })
 }
