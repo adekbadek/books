9c4f28d0c795e060fdf73c6e583df4827862173f
Adam Borowski <adam@adamboro.com>
2017-05-10 23:52:44 -0400

    move App to index

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
+
 import './styles/index.css'
+
+import Auth from 'views/Auth'
+import Main from 'views/Main'
+
+class App extends React.Component {
+  render () {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={Main} />
+          <Route path='/auth' component={Auth} />
+        </div>
+      </Router>
+    )
+  }
+}
 
 ReactDOM.render(
   <App />,
