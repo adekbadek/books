ba37e42cca4985f432c2354d3bb9fb97e18a2bb1
Adam Borowski <adam@adamboro.com>
2017-06-03 20:28:22 +0200

    Filters - refactor to functional component

--- client/src/components/Filters.jsx
+++ client/src/components/Filters.jsx
@@ -6,36 +6,32 @@ import { FILTERS, FILTER_NAMES } from 'utils/aux.js'
 import actions from 'store/actions'
 const { setFilterInput, setFilterType } = actions
 
-@connect(
+const Filters = props => {
+  const handleInputChange = e => {
+    props.setFilterInput(e.target.value)
+  }
+  return (
+    <div>
+      <input type='text' placeholder='filter' value={props.filterInput || ''} onChange={handleInputChange} />
+      <div className='mt2'>
+        {FILTER_NAMES.map((name, i) => (
+          <button
+            className={`${borderButtonClasses} mr2 ${props.filterType === name ? 'filter--active' : ''}`}
+            key={i}
+            onClick={() => { props.setFilterType(name) }}
+          >
+            {FILTERS[name].label}
+          </button>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default connect(
   state => ({
     filterInput: state.books.filterInput,
     filterType: state.books.filterType,
   }),
   { setFilterInput, setFilterType }
-)
-export default class Filters extends React.Component {
-  handleInputChange = e => {
-    this.props.setFilterInput(e.target.value)
-  }
-  handleFilterTypeChange = type => {
-    this.props.setFilterType(type)
-  }
-  render () {
-    return (
-      <div>
-        <input type='text' placeholder='filter' value={this.props.filterInput || ''} onChange={this.handleInputChange} />
-        <div className='mt2'>
-          {FILTER_NAMES.map((name, i) => (
-            <button
-              className={`${borderButtonClasses} mr2 ${this.props.filterType === name ? 'filter--active' : ''}`}
-              key={i}
-              onClick={() => { this.handleFilterTypeChange(name) }}
-            >
-              {FILTERS[name].label}
-            </button>
-          ))}
-        </div>
-      </div>
-    )
-  }
-}
+)(Filters)
