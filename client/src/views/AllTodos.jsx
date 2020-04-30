cf684b8b08e6528136b944ce82bc30501fead39d
Adam Borowski <adam@adamboro.com>
2020-04-30 22:16:25 +0200

    feat(todos): render completed separately

--- client/src/views/AllTodos.jsx
+++ client/src/views/AllTodos.jsx
@@ -1,5 +1,6 @@
 // @flow
 
+import { groupBy } from 'ramda'
 import React, { useEffect } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
 
@@ -11,19 +12,33 @@ import { todosCollection } from 'store/selectors'
 const AllTodos = ({ bookId }) => {
   const dispatch = useDispatch()
   const todos = useSelector(todosCollection())
+
+  const { active = [], completed = [] } = groupBy(
+    todo => (todo.is_completed ? 'completed' : 'active'),
+    todos
+  )
+
   useEffect(() => {
     dispatch(todosActions.fetchAllTodos())
   }, [])
 
+  const renderSingle = todo => (
+    <tr key={todo.id}>
+      <td>
+        <SingleTodo todo={todo} withRemoveButtons />
+      </td>
+    </tr>
+  )
+
   return (
     <Table wrapperClassName='pb4'>
-      {todos.map(todo => (
-        <tr key={todo.id}>
-          <td>
-            <SingleTodo todo={todo} withRemoveButtons />
-          </td>
-        </tr>
-      ))}
+      {active.map(renderSingle)}
+      <tr>
+        <td>
+          <div className='mt4' />
+        </td>
+      </tr>
+      {completed.map(renderSingle)}
     </Table>
   )
 }
