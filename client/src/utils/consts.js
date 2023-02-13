4bf65e8553c51f97c7266670400e6c4ad159b89c
Adam Borowski <adam@adamboro.com>
2023-02-13 09:59:21 +0100

    feat: change domain name

--- client/src/utils/consts.js
+++ client/src/utils/consts.js
@@ -1,6 +1,6 @@
 // @flow
 
-export const PROD_API_URL = 'https://books-api.adamboro.com'
+export const PROD_API_URL = 'https://books-api.adamcassis.com'
 export const API_VERSION = 'v1'
 
 export const COLOR = '#00ff00'
